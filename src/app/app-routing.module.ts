eed989381aead23bcbdff0ea1cc8ce6d671f5ff2
thibaut schmutz <thibaut.schmu@gmail.com>
2020-11-05 17:48:39 +0100

    :sparkles: (AuthModule) create AuthModule, add components (login and signup), service and model create routes, lazyload this module into AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FourOhFourComponent } from './core/components/four-oh-four/four-oh-four.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: AppComponent },
+  {
+    path: 'connect',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  { path: '**', component: FourOhFourComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
