836a397ffdb9b0d88ee529a1b4f609a0aecda589
thibaut schmutz <thibaut.schmu@gmail.com>
2020-11-06 13:23:50 +0100

    :zap: (SharedModule) create a sharedModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [AuthRoutingModule, SharedModule],
+  providers: [AuthService],
 })
 export class AuthModule {}
