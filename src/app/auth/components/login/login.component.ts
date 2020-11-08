a199e0dc16fed952bf36b7d28735898cfbf86909
thibaut schmutz <thibaut.schmu@gmail.com>
2020-11-08 15:06:26 +0100

    :sparkles: (loginComponent) create form, add validators and retrieve data on submit

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  public form: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, , Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
 
+  login() {
+    if (this.form.status === 'VALID') {
+      console.log('formulaire valid');
+      console.log(this.form.value);
+    } else if (this.form.status === 'INVALID') {
+      console.log('formulaire invalid');
+    }
+
+    // this.authService.login().subscribe(
+    //   (res) => {
+    //     console.log(res);
+    //     console.log(this.form);
+    //     this.form.reset();
+    //   },
+    //   (err) => console.log(err)
+    // );
+  }
 }
