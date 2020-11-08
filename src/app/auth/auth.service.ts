894e291c7eda1289fcf2ce2b8fedbfe835514ba7
thibaut schmutz <thibaut.schmu@gmail.com>
2020-11-08 15:36:07 +0100

    :sparkles: (authService) create AuthService with env variable in dev mode

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  public isAuth$ = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(userInfos) {
+    return this.http.post(`${environment.apiUrl}/users/login`, userInfos);
+  }
 }
