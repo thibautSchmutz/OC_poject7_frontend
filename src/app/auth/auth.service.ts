2d79664f1b12a8f14f77c00db883274de8678adb
thibaut schmutz <thibaut.schmu@gmail.com>
2020-11-08 17:04:02 +0100

    :sparkles: (token in localstorage) add token to localstorage through authService when loggedIn.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { LocalstorageService } from '../core/services/localstorage.service';
+import { BehaviorSubject } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +11,17 @@ import { environment } from '../../environments/environment';
 export class AuthService {
   public isAuth$ = new BehaviorSubject<boolean>(false);
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private localstorageService: LocalstorageService
+  ) {}
 
   login(userInfos) {
-    return this.http.post(`${environment.apiUrl}/users/login`, userInfos);
+    return this.http.post(`${environment.apiUrl}/users/login`, userInfos).pipe(
+      tap((res) => {
+        this.localstorageService.set('userId', res['userId']);
+        this.localstorageService.set('token', res['token']);
+      })
+    );
   }
 }
