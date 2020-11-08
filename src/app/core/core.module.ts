2588c196160cac54c27deda959a6e4cab8bb941a
thibaut schmutz <thibaut.schmu@gmail.com>
2020-11-08 15:44:58 +0100

    :sparkles: (localstorageService) create LocalstorageService to store token when authenticated

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FourOhFourComponent } from './components/four-oh-four/four-oh-four.component';
+import { LocalstorageService } from './services/localstorage.service';
 
 @NgModule({
   declarations: [NavbarComponent, FourOhFourComponent],
   imports: [CommonModule],
   exports: [NavbarComponent, FourOhFourComponent],
+  providers: [LocalstorageService],
 })
 export class CoreModule {}
