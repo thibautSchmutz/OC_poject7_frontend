b66364d2850779ef07ce0ff688c5e25387d6655b
thibaut schmutz <thibaut.schmu@gmail.com>
2020-11-08 19:04:21 +0100

    :zap: (authService) improve authService by stricly typing the server response

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { LocalstorageService } from '../core/services/localstorage.service';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { tap, map } from 'rxjs/operators';
 
 @Injectable({
@@ -16,13 +16,18 @@ export class AuthService {
     private localstorageService: LocalstorageService
   ) {}
 
-  login(userInfos) {
-    return this.http.post(`${environment.apiUrl}/users/login`, userInfos).pipe(
-      tap((res) => {
-        this.localstorageService.set('userId', res['userId']);
-        this.localstorageService.set('token', `Bearer ${res['token']}`);
-        this.isAuth$.next(true);
-      })
-    );
+  login(userInfos): Observable<{ userId: string; token: string }> {
+    return this.http
+      .post<{ userId: string; token: string }>(
+        `${environment.apiUrl}/users/login`,
+        userInfos
+      )
+      .pipe(
+        tap((res) => {
+          this.localstorageService.set('userId', res.userId);
+          this.localstorageService.set('token', `Bearer ${res.token}`);
+          this.isAuth$.next(true);
+        })
+      );
   }
 }
