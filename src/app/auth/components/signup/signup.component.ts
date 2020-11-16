7753267081854886fb936dab1cff2665cdebf6cb
thibaut schmutz <thibaut.schmu@gmail.com>
2020-11-16 15:30:56 +0100

    :sparkles: (Signup) create signup component : UI and error managment

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Form,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,7 +13,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-  constructor() {}
+  // RECUPERATION ELEMENTREF
+  @ViewChild('passwordToggle') passwordToggle: ElementRef;
 
-  ngOnInit(): void {}
+  // DECLARATION FORMULAIRE
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // CREATION FORMULAIRE
+    this.form = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'),
+        ],
+      ],
+      imageUrl: [''],
+    });
+  }
+
+  // GETTERS - FORM CONTROLS
+  get firstName() {
+    return this.form.get('firstName');
+  }
+  get lastName() {
+    return this.form.get('lastName');
+  }
+  get email() {
+    return this.form.get('email');
+  }
+  get imageUrl() {
+    return this.form.get('imageUrl');
+  }
+
+  // AFFICHER MOT DE PASSE
+  showPassword(password) {
+    if (password.type === 'password') {
+      password.type = 'text';
+      this.passwordToggle.nativeElement.classList.replace(
+        'hide-password',
+        'show-password'
+      );
+    } else {
+      password.type = 'password';
+      this.passwordToggle.nativeElement.classList.replace(
+        'show-password',
+        'hide-password'
+      );
+    }
+  }
+
+  // SUBMIT
+  login() {
+    // ENVOI AU SERVEUR
+    if (this.form.valid) {
+      console.log('envoi au serveur');
+    }
+  }
 }
