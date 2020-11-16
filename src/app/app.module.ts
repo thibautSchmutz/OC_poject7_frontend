4e833aeb14d0e451a5d62f35a4fabb739b945043
thibaut schmutz <thibaut.schmu@gmail.com>
2020-11-16 16:53:46 +0100

    :sparkles: (angular Material) add Angular Mateirla to the project + create a custom material-theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { CoreModule } from './core/core.module';
     RouterModule,
     CoreModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
