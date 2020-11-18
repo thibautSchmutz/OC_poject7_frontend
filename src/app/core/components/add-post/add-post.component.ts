e66b16bae04fd78b60719741a31418e5dadef6c4
thibaut schmutz <thibaut.schmu@gmail.com>
2020-11-18 17:28:41 +0100

    :sparkles: (add Post Button) create addPost Component to be able to post from anywhere in the app

---
+++ src/app/core/components/add-post/add-post.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+// Modal
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../../../shared/components/modal/modal.component';
+
+@Component({
+  selector: 'app-add-post',
+  templateUrl: './add-post.component.html',
+  styleUrls: ['./add-post.component.scss'],
+})
+export class AddPostComponent implements OnInit {
+  public isAuth$: BehaviorSubject<boolean>;
+
+  constructor(private authService: AuthService, private matDialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.isAuth$ = this.authService.isAuth$;
+  }
+
+  openModal() {
+    this.matDialog.open(ModalComponent, { data: { addPost: true } });
+  }
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,16 +4,28 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FourOhFourComponent } from './components/four-oh-four/four-oh-four.component';
 import { LocalstorageService } from './services/localstorage.service';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { TokenInterceptor } from './interceptors/token.interceptor';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
+import { AddPostComponent } from './components/add-post/add-post.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { PostService } from '../post/post.service';
 
 @NgModule({
-  declarations: [NavbarComponent, FourOhFourComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, FourOhFourComponent],
+  declarations: [NavbarComponent, FourOhFourComponent, AddPostComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [NavbarComponent, FourOhFourComponent, AddPostComponent],
   providers: [
     AuthService,
+    PostService,
     LocalstorageService,
     {
       provide: HTTP_INTERCEPTORS,
