8254285ba69d3d63e210258ea3d5f8043fb8a62b
thibaut schmutz <thibaut.schmu@gmail.com>
2020-11-18 17:32:54 +0100

    :sparkles: (modal) add basic template and logic for user to post through the addPost button

--- src/app/post/components/post-list/post-list.component.ts
+++ src/app/post/components/post-list/post-list.component.ts
@@ -13,19 +13,9 @@ import { Router } from '@angular/router';
 export class PostListComponent implements OnInit {
   public posts$: Observable<Post[]>;
 
-  constructor(
-    private postService: PostService,
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  constructor(private postService: PostService, private router: Router) {}
 
   ngOnInit(): void {
-    // Si le user n'est pas authentifié, on le redirige vers la page de connexion
-    // Sinon, on charge les posts dans un observable et on subscribe dans le template via le pipe async
-    if (!this.authService.isAuth$.value) {
-      this.router.navigate(['connect']);
-    } else {
-      this.posts$ = this.postService.getAllPosts();
-    }
+    this.posts$ = this.postService.getAllPosts();
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatDialogModule } from '@angular/material/dialog';
+// import { MatDialogModule } from '@angular/material/dialog';
 import { ModalComponent } from './components/modal/modal.component';
 import { MatButtonModule } from '@angular/material/button';
+import { PostFormComponent } from './components/post-form/post-form.component';
 // import { ImageUploadComponent } from './components/image-upload/image-upload.component';
 
 @NgModule({
-  declarations: [ModalComponent],
-  imports: [CommonModule, MatDialogModule, MatButtonModule, RouterModule],
-  exports: [MatDialogModule, ModalComponent, MatButtonModule],
+  declarations: [ModalComponent, PostFormComponent],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [ModalComponent, MatButtonModule, FormsModule, ReactiveFormsModule],
 })
 export class SharedModule {}
